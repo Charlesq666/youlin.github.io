0a5035e6eb65de2cf7696611e732bc8f8908ea72
charles <yqu39@wisc.edu>
2023-05-06 21:10:01 -0500

    feat: landing page

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,7 +1,30 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Portfolio</title>
+      </Head>
+    
+      <Header />
+    
+      {/* HERO */}
+      <section id='hero' className="snap-center">
+        <Hero />
+      </section>
+
+      {/* ABOUT ME */}
+
+      {/* EXPERIENCE */}
+
+      {/* SKILLS */}
+
+      {/* PROJECTS */}
+
+      {/* CONTACT */}
     </div>
   )
 }
