06b9aaea10263b24d8552cc4bd696c3b108e92ff
charles <yqu39@wisc.edu>
2023-06-02 00:05:11 -0500

    build: google analitics

--- portfolio/pages/_document.tsx
+++ portfolio/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript} from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,6 +10,22 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-TR21HP6L6S"
+            strategy="afterInteractive"
+          />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-GQ1WVD16MQ');
+              `,
+            }}
+          />
       </body>
     </Html>
   )
