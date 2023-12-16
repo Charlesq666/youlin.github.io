4c6258af7237792c48f077319a74d3e9d344c956
charles <yqu39@wisc.edu>
2023-12-16 12:37:43 -0500

    test github action

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const isProd = process.env.NODE_ENV === 'production';
+console.log(`isProd: ${isProd}`);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  /* config options here */ 
  typescript: {
-    basePath: isProd ? '/youlin.github.io' : '',
-    assetPrefix: isProd ? '/youlin.github.io/' : '',
     ignoreBuildErrors: true,
   },
+  basePath: isProd ? '/youlin.github.io' : '',
+  assetPrefix: isProd ? '/youlin.github.io/' : '',
 };
 
 module.exports = nextConfig
