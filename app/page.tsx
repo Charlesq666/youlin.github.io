9219cdf616d14735d92d0325fb76fa0c73d262d0
charles <yqu39@wisc.edu>
2023-12-16 15:26:04 -0500

    test github action

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src={`${process.env.IMG_PREFIX}/vercel.svg`}
+              src={`/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
