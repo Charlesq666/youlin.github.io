ab55d71838cda5c0c7c3d65e172584404738d895
charles <yqu39@wisc.edu>
2023-12-16 18:43:35 -0500

    nav bar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,4 @@
-'use client'
-import { useRouter } from "next/navigation"
 
 export default function Dashboard() {
-    const router = useRouter()
-    router.push('/')
     return <div>Dashboard</div>
 }
