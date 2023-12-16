857a38d0a4cbb62652e0774a99704ee9f1846f98
charles <yqu39@wisc.edu>
2023-12-16 15:23:38 -0500

    test github action

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={`${process.env.IMG_PREFIX}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    env: {
+        IMG_PREFIX : isProd ? 'youlin.github.io' : ''
+    }
 }
 
 module.exports = nextConfig
