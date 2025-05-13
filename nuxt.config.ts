b8f02c373a57643a14e75a080ef040f342735197
Charles Qu <chalice.chu@gmail.com>
2025-05-12 22:54:48 -0700

    minor config change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ["~/assets/css/main.css"],
+  ui: {
+    prefix: "U"
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/ui"]
 })
